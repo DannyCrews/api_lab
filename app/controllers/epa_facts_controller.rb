23a3d0f13f3cf384f1a03b102912559b24e1a6e4
Dan Crews <dancrews@me.com>
2013-11-12 13:50:07 -0500

    Merge with Abby's version.

--- app/controllers/epa_facts_controller.rb
+++ app/controllers/epa_facts_controller.rb
@@ -7,9 +7,14 @@ class EpaFactsController < ApplicationController
   
 # website http://www.epa.gov/enviro/facts/ghg/summary_model.html
 
-	def index
-    @facilities = Facility.all
-  end
+def index
+    @main_data = HTTParty.get('http://iaspub.epa.gov/enviro/efservice/PUB_DIM_FACILITY/ROWS/0:100/XML')
+    @emissions_data = HTTParty.get('http://iaspub.epa.gov/enviro/efservice/PUB_FACTS_SUBP_GHG_EMISSION/ROWS/0:1000/XML')
+end
+
+	# def index
+ #    @facilities = Facility.all
+ #  end
 
 
   # def facilities.js
