9d3cfe8f920ff0f9358504fc37c2d235771a9c3a
Matt Clement <darkmoves@gmail.com>
2013-11-13 14:43:59 -0500

    Deleted references to Zerb Foundation for now. Added Ajax call to fuel_graph.js.

--- app/assets/javascripts/fuel_graph.js
+++ app/assets/javascripts/fuel_graph.js
@@ -2,12 +2,19 @@
 //ajax, go to fuel controller index and fetch me my data, please
 // success: show me the data in my chosen format (or alert(data))
 
-function() = {
+var = FuelGraph {}
 
+var FuelGraph.getData = function() {
+	$.ajax({
+		url: '/fuel_graph',
+		type: 'GET',
+		dataType: 'json',
+		success: function(data) {
+			console.log(data);
+		}
+	});
 };
 
-
 $(document).ready(function() {
-
-
-})
+	FuelGraph.getData();
+});
