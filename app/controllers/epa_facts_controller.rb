230ea86319022ce5fef3b96f9dc6324be154902f
Dan Crews <dancrews@me.com>
2013-11-14 11:00:44 -0500

    Edited CSS

--- app/controllers/epa_facts_controller.rb
+++ app/controllers/epa_facts_controller.rb
@@ -4,7 +4,7 @@ class EpaFactsController < ApplicationController
 
   # require 'open-uri' # from nokogiri xml parsing tutorial
 
-  base_uri 'http://iaspub.epa.gov/enviro/efservice/PUB_DIM_FACILITY'
+  # base_uri 'http://iaspub.epa.gov/enviro/efservice/PUB_DIM_FACILITY'
   
 # website http://www.epa.gov/enviro/facts/ghg/summary_model.html
 
