53ec397b401e445953281b9689e96f0aea984d56
Matt Clement <darkmoves@gmail.com>
2013-11-14 09:34:28 -0500

    Styling for placeholder bar graph div.

--- app/assets/javascripts/fuel_graph.js
+++ app/assets/javascripts/fuel_graph.js
@@ -13,20 +13,8 @@ var FuelGraph = {
 		});
 	},
 
-	// getStateData: function() {
-	// 	$.ajax({
-	// 		url: '/fuel_graph'
-	// 		type: 'GET',
-	// 		dataType: 'json'
-	// 		success: function(data) {
-	// 			console.log(data)
-	// 		}
-	// 	})
-	// },
-
 	change: function() {
 		var dropdown = d3.select('#state_source');
-		debugger;
 		var state_abbr = dropdown.node().options[dropdown.node().selectedIndex].value;
 		d3.json(state_abbr, function(json) {
 			console.log(state_abbr); //Test that the state is passed
