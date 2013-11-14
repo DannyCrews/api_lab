889a14d6b3efe663193feca657c7c342c6dd5f37
Matt Clement <darkmoves@gmail.com>
2013-11-13 23:01:24 -0500

    Quick improvements.

--- app/controllers/fuel_breakdown_controller.rb
+++ app/controllers/fuel_breakdown_controller.rb
@@ -1,10 +1,6 @@
 class FuelBreakdownController < ApplicationController
 	respond_to :html, :json, :js
 
-	# def index
-	# 	respond_with FuelBreakdown.where(prod_type: "Total Electric Power Industry")		
-	# end
-
 	def index
 		state_abbr = params[:state_abbr]
 		respond_with FuelBreakdown.where(prod_type: "Total Electric Power Industry").where(state_abbr: state_abbr)
